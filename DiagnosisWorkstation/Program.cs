b9a1f9fa8482373b97b666fd4e29d567afabd7eb
烟雨斜阳墨如枫 <sunnypron@hotmail.com>
2018-05-27 21:39:05 +0800

    1、新建诊断工作站项目，创建主窗体。

--- DiagnosisWorkstation/Program.cs
+++ DiagnosisWorkstation/Program.cs
@@ -15,7 +15,7 @@ namespace DiagnosisWorkstation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
